75d4a088f70d6ec251a476da6363f9c71737797c
Quentin Lemaire <sec@quentinlemaire.fr>
2018-07-22 22:20:58 +0200

    Parse args with "commandlineparser"

--- Program.cs
+++ Program.cs
@@ -2,39 +2,90 @@
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using Registry;
+using CommandLine;
+using CommandLine.Text;
+using System.Linq;
+using System.IO;
 
 namespace HiveTransactionsMerger
 {
     class Program
     {
+        public class Options
+        {
+            [Option('d', "DirtyHive", Required = true, HelpText = "Dirty hive to process.")]
+            public string DirtyHiveFilename { get; set; }
+
+            /*
+             * [OptionArray('t', "Transactions", DefaultValue = new[] {""}, HelpText = "Transaction files to process")]
+             *  public string[] TransactionFilenames { get; set; }
+             */
+            [Option('t', "Transactions", Separator = ',', Required = true, HelpText = "Transaction files to process.")]
+            public IEnumerable<string> TransactionFilenames { get; set; }
+
+            [Option('o', "Output", Required = true, HelpText = "Output file")]
+            public string OutputFilename { get; set; }
+
+            [Option(Default = false, HelpText = "Prints all messages to standard output.")]
+            public bool Verbose { get; set; }
+
+            [Usage(ApplicationAlias = "HiveTransactionsMerger.exe")]
+            public static IEnumerable<Example> Examples
+            {
+            get
+                {
+                    yield return new Example("Standard", new Options { DirtyHiveFilename = "NTUSER.dat", TransactionFilenames = new List<string> { "NTUSER.dat.log1", "NTUSER.dat.log2" }, OutputFilename = "NTUSER.dat.merged" });
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args == null)
             {
-                Console.WriteLine("Usage: HiveTransactionMerger.exe <hive> <transactionFiles...>", args[0]);
-                Environment.Exit(1);
+                throw new ArgumentNullException(nameof(args));
             }
 
-            /* Arguments management */
-            var hiveFilename = args[1];
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(opts => Run(opts))
+                .WithNotParsed<Options>((errs) => HandleParseError(errs));
+        }
 
-            // dirty
-            var logTransactions = new List<string>();
-            foreach (var transactionFilename in args)
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            Environment.Exit(1);
+        }
+
+        static void Run(Options opts)
+        {
+            Console.WriteLine("Number of transactions logs: {0}", opts.TransactionFilenames.Count());
+            foreach (var trans in opts.TransactionFilenames)
             {
-                logTransactions.Add(transactionFilename);
+                Console.WriteLine("[i] Transaction file: {0}", trans);
             }
-            logTransactions.RemoveAt(0); // remove first arg <== it's the hive file
 
             /* Online: commitRegistry */
             // TODO
 
-            /* Offline: TODO */
-            var registryHive = new RegistryHive(hiveFilename);
-            byte[] mergedHive = registryHive.ProcessTransactionLogs(logTransactions);
+            /* Offline */
+            var registryHive = new RegistryHive(opts.DirtyHiveFilename);
+            byte[] mergedHive = registryHive.ProcessTransactionLogs(opts.TransactionFilenames.ToList());
 
-            Console.WriteLine("Merged {0} bytes", mergedHive.Length);
+            Console.WriteLine("[*] Merged {0} bytes", mergedHive.Length);
+            Console.WriteLine("[*] Writing to {0}", opts.OutputFilename);
+
+            using (var fs = new FileStream(opts.OutputFilename, FileMode.Create))
+            {
+                using (var bw = new BinaryWriter(fs))
+                {
+                    bw.Write(mergedHive);
+                }
+            }
+
+            Console.WriteLine("[*] Done");
         }
+
+
 
         [DllImport("Advapi32.dll", EntryPoint = "RegFlushKey")]
         public static extern int RegFlushKey(IntPtr hKey);
